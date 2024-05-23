404bcfe8faff4b86cb0db3a16b5b433e1a75522b
Marcos Parreiras <marcosalp1998@gmail.com>
2024-05-23 17:04:38 -0300

    init: create a spring boot project with spring-boot-web and spring-devtools dependecies

---
+++ src/main/java/com/marcosparreiras/fastfeet/FastfeetApplication.java
@@ -0,0 +1,13 @@
+package com.marcosparreiras.fastfeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfeetApplication.class, args);
+	}
+
+}
